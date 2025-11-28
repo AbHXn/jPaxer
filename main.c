03c21b80f851db8b2a3c249528d3fc475f5f1734
AbHXn <asofficial47@gmail.com>
2025-11-28 20:21:58 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "libs/jpaxer.h"
+
+int main( int argc, char *argv[] ){
+	FILE* jfile = fopen( "test_jsons/test1.json", "r" );
+	JSON_NODE* p_json = parse_JSON_from_FILE( jfile );
+	get( p_json, "configurations" );
+	get( p_json, "0" );
+	del( p_json, "id" );
+	back(p_json);
+	back(p_json);
+	print_JSON_node( p_json );
+	FILE* test = fopen( "final.json", "w");
+	JSON_FLUSH( p_json, test, 1 );
+	return 0;
+}
