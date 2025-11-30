5944ba522a5c525642f1069d0ff95e50f3807ca6
AbHXn <asofficial47@gmail.com>
2025-11-30 07:54:45 +0530

    deleted files

--- libs/jpaxer.c
+++ libs/jpaxer.c
@@ -1,5 +1,62 @@
 #include "jpaxer.h"
 
+ void print_node(JSON_NODE* node, int level) {
+    for (int i = 0; i < level; i++) printf("  "); // indentation
+    printf(C_KEY "\"%s\"" C_RESET ": ", node->key);
+    switch (node->dtype) {
+        case J_STRING:
+            printf(C_STRING "\"%s\"" C_RESET, node->value.string_val);
+            break;
+        case J_INT:
+            printf(C_NUMBER "%ld" C_RESET, *(node->value.int_val));
+            break;
+        case J_DOUBLE:
+            printf(C_NUMBER "%lf" C_RESET, *(node->value.double_val));
+            break;
+        case J_BOOL:
+            printf(C_BOOL "%s" C_RESET,
+                   *(node->value.bool_val) ? "true" : "false");
+            break;
+        case J_NULL:
+            printf(C_NULL "null" C_RESET);
+            break;
+        case J_OBJECT:
+            printf(C_BRACE "{\n" C_RESET);
+            jobject* cur = node->value.object_val;
+            while (cur) {
+                print_node(cur->j_node, level + 1);
+                if (cur->next) printf(",\n");
+                else printf("\n");
+                cur = cur->next;
+            }
+            for (int i = 0; i < level; i++) printf("  ");
+            printf(C_BRACE "}" C_RESET);
+            break;
+
+        default:
+            printf("(unknown)");
+    }
+}
+
+void print_JSON_node(JSON_NODE* root) {
+    if (!root) return;
+
+    printf(C_BRACE "{\n" C_RESET);
+    if( root->dtype != J_OBJECT ){
+        print_node( root, 1 );
+        printf("\n");
+    }else{
+        jobject* cur = root->value.object_val;
+        while (cur) {
+            print_node(cur->j_node, 1);
+            if (cur->next) printf(",\n");
+            else printf("\n");
+            cur = cur->next;
+        }
+    }
+    printf(C_BRACE "}\n" C_RESET);
+}
+
 void JSON_FLUSH(JSON_NODE* json_data, FILE* json_file, int indent) {
     if (!json_data) return;
 
--- libs/jpaxer.h
+++ libs/jpaxer.h
@@ -2,14 +2,22 @@
 #define JPAXER_H
 
 #include "parser.h"
-#include "display.h"
 #include <stdarg.h>
+#include <stdio.h>
 
+#define C_RESET   "\033[0m"
+#define C_KEY     "\033[1;36m"
+#define C_STRING  "\033[1;32m"
+#define C_NUMBER  "\033[1;33m"
+#define C_BOOL    "\033[1;35m"
+#define C_NULL    "\033[1;31m"
+#define C_BRACE   "\033[1;34m"
 
 typedef struct _json_traverser JSON_TR;
 typedef void (*jFUNC)(JSON_TR**,const char*);
 
 void   JSON_FLUSH(JSON_NODE* , FILE*, int);
+void   print_JSON_node( JSON_NODE* root );
 
 void   __read__	 ( JSON_NODE**, int, ...);
 void   __update__( JSON_NODE**, const char*, void*, JSON_DTYPE );
@@ -22,4 +30,5 @@ void   __back__	 ( JSON_NODE**, int);
 #define add(j_node,key,value,dtype) __create__((&j_node),(key),(value),(dtype))
 #define del(j_node,key)				__delete__((&j_node),(key))
 #define back(j_node,step)			__back__((&j_node),(step))
+
 #endif
